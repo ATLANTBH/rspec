7a1074d73a16d2fab07bbd455eaf10bc8f43f4f2
Nermin Caluk <nermin.caluk@gmail.com>
2013-02-21 12:55:37 +0100

    rspec database formatter

---
+++ lib/dbformatter.rb
@@ -0,0 +1,142 @@
+require 'rspec/core/formatters/base_text_formatter'
+require 'active_record'
+require 'yaml'
+# require_relative 'connectdb'
+
+=begin 
+ 
+  see http://rdoc.info/github/rspec/rspec-core/RSpec/Core/Formatters/BaseFormatter
+  The reporter calls every formatter with this protocol:
+  
+    start(expected_example_count)
+    zero or more of the following
+      example_group_started(group)
+      example_started(example)
+      example_passed(example)
+      example_failed(example)
+      example_pending(example)
+      message(string)
+    stop
+    start_dump
+    dump_pending
+    dump_failures
+    dump_summary(duration, example_count, failure_count, pending_count)
+    seed(value)
+    close
+
+=end
+
+ 
+class TestCase < ActiveRecord::Base
+  belongs_to :testrun
+end
+
+class TestRun < ActiveRecord::Base
+  has_many :testcases
+  belongs_to :testsuite
+end
+
+class TestSuite < ActiveRecord::Base
+  has_many :testruns
+end
+
+class DBFormatter < RSpec::Core::Formatters::BaseTextFormatter
+  
+    attr_reader :output, :results, :example_group
+
+    def initialize(output)
+      @output = output || StringIO.new
+      @results = {} 
+      
+      @config = YAML::load(File.open('./config/config.yml'))
+      
+      ActiveRecord::Base.establish_connection(@config["dbconnection"])
+      @testrun = TestRun.create()
+      @testsuite = TestSuite.find_or_create_by_suite(:suite=>@config["options"]["suite"])
+    end
+    
+    def insert_test_case(example)
+      @testcase = TestCase.create(
+        :testrun_id=>@testrun.id,
+        :test_group=>@example_group.display_name,
+        :description=>example.description,
+        :execution_result=>example.execution_result[:status],
+        :duration=>example.execution_result[:run_time],
+        :pending_message=>example.execution_result[:pending_message].to_s,
+        :exception=>example.execution_result[:exception].to_s
+        )
+      
+      if @config["options"]["backtrace"] == "ON" 
+        @testcase.update_attributes(
+          :backtrace=>example.execution_result[:backtrace], #fix 
+          :metadata=>example.metadata
+        )
+      end
+    end  
+    
+    def insert_test_run(duration, example_count, failure_count, pending_count)
+       @testrun.update_attributes(
+        :testsuite_id=>@testsuite.id,
+        :duration=>duration, 
+        :example_count=>example_count, 
+        :failure_count=>failure_count, 
+        :pending_count=>pending_count,
+        :build=>@config["options"]["build"]) 
+    end
+
+    def start(example_count)
+    end
+
+    def example_group_started(example_group)
+      @example_group=example_group
+    end
+
+    def example_group_finished(example_group)
+    end
+
+    def example_started(example) 
+    end
+
+    def example_passed(example)
+      insert_test_case(example)
+    end
+
+    def example_pending(example)
+      insert_test_case(example)
+    end
+
+    def example_failed(example)
+      insert_test_case(example)
+    end
+
+    def message(message)
+      @message=message
+    end
+
+    def stop
+    end
+
+    def start_dump()
+    end
+
+    def dump_pending()
+    end
+
+    def dump_failures()
+      
+    end
+
+    def dump_summary(duration, example_count, failure_count, pending_count)
+      insert_test_run(duration, example_count, failure_count, pending_count)
+    end
+
+    # no-op
+    def seed(seed)
+    end
+
+    def close()
+    end
+ 
+
+
+end
---
+++ spec/spec_spec.rb
@@ -0,0 +1,24 @@
+describe "Dummy" do
+  
+  context "kontekst" do
+  
+  it "does nothing" do
+    1.should eql(1)
+  end
+  
+    end 
+  
+  it "does nothing again" do
+    1.should eql(1)
+  end
+  
+
+  
+  it "raises" do
+    raise "oh no"
+  end
+  
+  it "pends" do pending
+  end
+  
+end
