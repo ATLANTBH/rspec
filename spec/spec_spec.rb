ef16bc94a2ab961f39d032e34923d0beee1044b3
Nermin Caluk <nermin.caluk@gmail.com>
2013-02-25 12:44:45 +0100

    test spec updated

--- spec/spec_spec.rb
+++ spec/spec_spec.rb
@@ -1,18 +1,14 @@
-describe "Dummy" do
+describe "Dummy describe" do # this spec file is just for test / demo purposes
   
-  context "kontekst" do
-  
-  it "does nothing" do
-    1.should eql(1)
-  end
-  
-    end 
+  context "CoNtExT" do
+    it "does nothing, within context" do
+      1.should eql(1)
+    end
+  end 
   
   it "does nothing again" do
     1.should eql(1)
   end
-  
-
   
   it "raises" do
     raise "oh no"
